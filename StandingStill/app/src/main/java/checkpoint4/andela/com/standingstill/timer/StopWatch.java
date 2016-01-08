b7f650b7c211ac42aff5d0937ce9a631ee8d35eb
andela-djames <daniel.james@andela.com>
2016-01-08 07:08:06 +0100

    Add method to Stopwatch.java

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/timer/StopWatch.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/timer/StopWatch.java
@@ -4,7 +4,18 @@ package checkpoint4.andela.com.standingstill.timer;
  * Created by andeladev on 07/01/2016.
  */
 public class StopWatch {
+
+    private long hour;
+    private long minute;
+    private long seconds;
+    private long milliSeconds;
+
+    public StopWatch() {
+    }
+
+    public void updateTimer() {
+
+    }
+
     
-
-
 }
