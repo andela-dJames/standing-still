bebf55053428f78f37d07794dd530dc7b15badfe
andela-djames <daniel.james@andela.com>
2016-01-08 14:13:51 +0100

    Implement Stopwatch

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/timer/StopWatch.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/timer/StopWatch.java
@@ -1,21 +1,66 @@
 package checkpoint4.andela.com.standingstill.timer;
 
+import android.os.Handler;
+
 /**
  * Created by andeladev on 07/01/2016.
  */
 public class StopWatch {
 
-    private long hour;
-    private long minute;
-    private long seconds;
+    private long hr;
+    private long min;
+    private long sec;
     private long milliSeconds;
+    private Handler handler;
+    private long startTime, elapsedTime;
+
+    private int refreshRate = 100;
+
+    private String seconds, minute, hour;
+    private boolean stopped;
+
+//    private Runnable startTimer = new Runnable() {
+//        @Override
+//        public void run() {
+//            elapsedTime = System.currentTimeMillis() - startTime;
+//            updateTimer();
+//            handler.postDelayed(this, refreshRate);
+//        }
+//    };
+
+
+
+
+
 
     public StopWatch() {
     }
 
-    public void updateTimer() {
+    public void updateTimer(float time) {
+        sec = (long) time / 1000;
+        min = sec / 60;
+        hr = min / 60;
 
     }
 
+    public String getSeconds(float time) {
+        updateTimer(time);
+        sec = sec % 60;
+        seconds=String.valueOf(sec);
+        if(sec == 0){
+            seconds = "00";
+        }
+        if(sec <10 && sec > 0){
+            seconds = "0"+seconds;
+        }
+        return seconds;
+    }
 
+    public String getMinute() {
+        return minute;
+    }
+
+    public String getHour() {
+        return hour;
+    }
 }
