43b6d2fa07bfe61cc6d87cefce5e27c45a22bcbc
andela-djames <daniel.james@andela.com>
2016-01-09 03:57:38 +0100

    Add GoogleLocationService.java

---
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/UserLocationService.java
@@ -0,0 +1,54 @@
+package checkpoint4.andela.com.standingstill;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by andeladev on 09/01/2016.
+ */
+public class UserLocationService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private final static String TAG = "Location Activity";
+
+    private GoogleApiClient apiClient;
+    private Location location;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
