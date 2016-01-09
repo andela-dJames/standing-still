0564bbc445a15212d5d430db0492d3805af2bde6
andela-djames <daniel.james@andela.com>
2016-01-09 04:19:35 +0100

    Add Location Activity

---
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/LocationFragment.java
@@ -0,0 +1,30 @@
+package checkpoint4.andela.com.standingstill;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LocationFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_fragment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
---
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/LocationFragmentFragment.java
@@ -0,0 +1,22 @@
+package checkpoint4.andela.com.standingstill;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LocationFragmentFragment extends Fragment {
+
+    public LocationFragmentFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_location_fragment, container, false);
+    }
+}
