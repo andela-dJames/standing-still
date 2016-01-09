fa2effaefdec9cc4417c4196a2ec519369bc8580
andela-djames <daniel.james@andela.com>
2016-01-09 04:22:08 +0100

    Remove FAB

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/LocationFragment.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/LocationFragment.java
@@ -15,16 +15,7 @@ public class LocationFragment extends AppCompatActivity {
         setContentView(R.layout.activity_location_fragment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
     }
 
 }
