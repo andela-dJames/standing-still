2aa1e1b149350aedc45ec812eff520590264e634
andela-djames <daniel.james@andela.com>
2016-01-10 23:03:18 +0100

    Add timeSpent method

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/timer/StopWatch.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/timer/StopWatch.java
@@ -90,6 +90,37 @@ public class StopWatch {
 
         return hour;
     }
+
+    public String timeSpent(long time) {
+        String initial = "";
+        String message = "";
+        initial = hourToString();
+        if (initial.equals("00")){
+            message +="";
+        }
+        else {
+            message += initial + " hrs, ";
+        }
+
+        initial = minuteToString();
+        if (initial.equals("00")){
+            message +="";
+        }
+        else {
+            message += initial + " min, ";
+        }
+
+        initial = secondsToString();
+
+        if (initial.equals("00")){
+            message += "";
+        }
+        else {
+            message += initial + " sec";
+        }
+
+        return message;
+    }
     
 
     public String getHour() {
