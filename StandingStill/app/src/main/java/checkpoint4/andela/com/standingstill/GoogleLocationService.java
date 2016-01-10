b9d44b522c591337946e98dba29a9817ef5c3cdc
andela-djames <daniel.james@andela.com>
2016-01-10 22:57:21 +0100

    Implement Google API client callbacks

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/GoogleLocationService.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/GoogleLocationService.java
@@ -1,9 +1,12 @@
 package checkpoint4.andela.com.standingstill;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -13,17 +16,23 @@ import com.google.android.gms.location.LocationServices;
 /**
  * Created by andeladev on 09/01/2016.
  */
-public class GoogleLocationService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class GoogleLocationService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private final static String TAG = "Location Activity";
 
     private GoogleApiClient googleApiClient;
     private LocationRequest locationrequest;
+    private Location userLocation;
     private Context context;
+
+    private double longitude;
+    private double latitude;
 
     public GoogleLocationService(Context context) {
         this.context = context;
         buildApiClient();
+        longitude = 0;
+        latitude = 0;
     }
 
     private void buildApiClient() {
@@ -39,11 +48,42 @@ public class GoogleLocationService implements GoogleApiClient.ConnectionCallback
     public void onConnected(Bundle bundle) {
         locationrequest = LocationRequest.create();
         locationrequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationrequest.setInterval(1000);
-        
+        locationrequest.setInterval(100);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationrequest, this);
 
 
 
+
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    private int checkSelfPermission(Object accessFineLocation) {
+        return 0;
     }
 
     @Override
@@ -54,20 +94,9 @@ public class GoogleLocationService implements GoogleApiClient.ConnectionCallback
     @Override
     public void onLocationChanged(Location location) {
 
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
+        Log.d(TAG, location.toString());
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
 
     }
 
