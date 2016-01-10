be3b0cc59b5357d7265301e359b99280dcebc35e
andela-djames <daniel.james@andela.com>
2016-01-10 22:59:28 +0100

    Receive Intent From Main Method and Change how GoogleAPIClient is implemented in App

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/LocationFragment.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/LocationFragment.java
@@ -1,18 +1,33 @@
 package checkpoint4.andela.com.standingstill;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationFragment extends AppCompatActivity {
     private final static String TAG = "Record Location";
 
-    private GoogleLocationService googleLocationService;
+    private TextView view;
+    private TextView latitiudeText;
+    private GoogleApiClient googleApiClient;
+    private double longitude;
+    private double latitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +36,40 @@ public class LocationFragment extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        googleLocationService = new GoogleLocationService(this);
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            LocationFragmentFragment fragment = new LocationFragmentFragment();
+            fragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+
+        }
+
+        view = (TextView) findViewById(R.id.long_text);
+        latitiudeText = (TextView) findViewById(R.id.lat_text);
+        receiveIntent(savedInstanceState);
+
+    }
+
+
+    public void receiveIntent(Bundle bundle) {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("TIMESPENT");
+        String user = intent.getStringExtra("LATITUDE");
+        TextView txt = new TextView(this);
+        txt.setTextSize(40);
+        txt.setText(message);
+        view.setText(message);
+        latitiudeText.setText(user);
+
 
 
     }
@@ -34,15 +82,15 @@ public class LocationFragment extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        googleLocationService.connect();
+
     }
 
     @Override
     protected void onStop() {
-
-        if (googleLocationService.isConnected()){
-            googleLocationService.disconnect();
-        }
         super.onStop();
+
     }
+
+
+
 }
