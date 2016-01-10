bda62e6843d4aacb1a717a169f6c93baef87134d
andela-djames <daniel.james@andela.com>
2016-01-11 00:33:17 +0100

    Implement onHandleIntent callback

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/DetectedActivities.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/DetectedActivities.java
@@ -2,6 +2,12 @@ package checkpoint4.andela.com.standingstill;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
 
 /**
  * Created by andeladev on 10/01/2016.
@@ -20,6 +26,13 @@ public class DetectedActivities extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+        Intent localIntent = new Intent(Constants.BROADCAST_ACTION);
+
+        ArrayList<DetectedActivity> detectedActivities = (ArrayList) result.getProbableActivities();
+
+        localIntent.putExtra(Constants.ACTIVITY_EXTRA, detectedActivities);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
 
     }
 }
