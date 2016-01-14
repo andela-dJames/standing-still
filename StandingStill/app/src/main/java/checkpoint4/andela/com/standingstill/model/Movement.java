03cb6af2cf43b0a0f304ef3197ca757831f17b2a
andela-djames <daniel.james@andela.com>
2016-01-14 12:26:53 +0100

    Add properties with their getter/setter

--- StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/model/Movement.java
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/model/Movement.java
@@ -1,11 +1,67 @@
 package checkpoint4.andela.com.standingstill.model;
 
-/**
- * Created by andeladev on 14/01/2016.
- */
+import org.joda.time.DateTime;
+
 public class Movement {
 
     public enum Type{
+        STILL,
+        IN_VEHICLE,
+        ON_FOOT,
+        ON_BICYCLE,
+        WALKING,
+        RUNNING,
+        TILTING,
+        UNKNOWN
+    }
 
+
+    private Type movementType;
+
+    private long timeSpent;
+
+    private DateTime date;
+
+    private String address;
+
+    public Movement() {
+    }
+
+    public Movement(long timeSpent, DateTime date, String address) {
+        this.timeSpent = timeSpent;
+        this.date = date;
+        this.address = address;
+    }
+
+    public Type getMovementType() {
+        return movementType;
+    }
+
+    public void setMovementType(Type movementType) {
+        this.movementType = movementType;
+    }
+
+    public long getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(long timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
