8e57fcbe77b0e19da77e6bb7550583afdc134f1d
andela-djames <daniel.james@andela.com>
2016-01-14 11:09:51 +0100

    Add new packages for models and DAL

---
+++ StandingStill/app/src/main/java/checkpoint4/andela/com/standingstill/model/Place.java
@@ -0,0 +1,16 @@
+package checkpoint4.andela.com.standingstill.model;
+
+/**
+ * Created by andeladev on 14/01/2016.
+ */
+public class Place {
+
+    private String Address;
+
+    private double longitude;
+
+    private double latitude;
+
+    private long timeSpent;
+    
+}
