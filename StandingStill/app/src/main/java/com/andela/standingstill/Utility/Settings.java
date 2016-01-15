df2e23cb7f77e35a0e41a3f67abf1970f854bda8
andela-djames <daniel.james@andela.com>
2016-01-15 17:28:04 +0100

    Add utility package and Settings.java

---
+++ StandingStill/app/src/main/java/com/andela/standingstill/Utility/Settings.java
@@ -0,0 +1,26 @@
+package com.andela.standingstill.Utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by andeladev on 15/01/2016.
+ */
+public class Settings {
+
+    private Context context;
+
+    private SharedPreferences sharedPreferences;
+
+    public Settings(Context context) {
+        this.context = context;
+    }
+
+    public void saveSettings() {
+
+    }
+
+    public void retrieveSettings() {
+        
+    }
+}
