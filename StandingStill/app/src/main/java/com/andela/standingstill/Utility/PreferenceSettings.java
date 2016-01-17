f33f587d48d026ca22e5aa4b20609f44b1e0ffb1
andela-djames <daniel.james@andela.com>
2016-01-17 04:33:31 +0100

    Move TimeDuration to a separate class

--- StandingStill/app/src/main/java/com/andela/standingstill/Utility/PreferenceSettings.java
+++ StandingStill/app/src/main/java/com/andela/standingstill/Utility/PreferenceSettings.java
@@ -1,10 +1,13 @@
 package com.andela.standingstill.Utility;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.preference.DialogPreference;
 import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.TimePicker;
 
@@ -12,7 +15,7 @@ import com.andela.standingstill.R;
 
 
 public class PreferenceSettings extends DialogPreference implements Preference.OnPreferenceChangeListener {
-    private static final String DEFAULT_VALUE = "0:5";
+    public static final String DEFAULT_VALUE = "0:5";
     private TimePicker timePicker;
     protected TimeDuration timeDuration;
 
@@ -32,13 +35,13 @@ public class PreferenceSettings extends DialogPreference implements Preference.O
         int minute = timePicker.getCurrentMinute();
         if (positiveResult) {
             TimeDuration duration = new TimeDuration(hour, minute);
-            String savedVal = duration.timeToString();
+            String savedVal = duration.toString();
 
             if (callChangeListener(savedVal)) {
                 timeDuration = duration;
                 persistString(savedVal);
             }
-
+            Log.d("DELAY SET TO", savedVal);
         }
 
 
@@ -52,7 +55,8 @@ public class PreferenceSettings extends DialogPreference implements Preference.O
         } else {
             // Set default state from the XML attribute
             timeDuration = parseTimeDuration((String) defaultValue);
-            persistString(timeDuration.timeToString());
+            Log.d("TAG", timeDuration.toString());
+            persistString(timeDuration.toString());
         }
     }
 
@@ -85,91 +89,30 @@ public class PreferenceSettings extends DialogPreference implements Preference.O
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         setSummary(parseTimeDuration(newValue.toString()).timeToString());
+        retreiveSettings();
         return true;
     }
 
     public TimeDuration parseTimeDuration(String durationString) {
         String[] args = durationString.split(":");
+       TimeDuration duration = null;
+        if (args.length < 2){
+            return new TimeDuration(0, 5);
+        }
+            try {
+                return new TimeDuration(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            } catch (NumberFormatException e) {
 
-        return new TimeDuration(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-
+            }
+        return new TimeDuration(0, 5);
     }
 
-
-    public class TimeDuration {
-
-        protected int hour;
-
-        protected int minute;
-
-        protected int seconds;
-
-        public TimeDuration(int hour, int minute) {
-            this.hour = hour;
-            this.minute = minute;
-        }
-
-        public TimeDuration(int hour, int minute, int seconds) {
-            this.hour = hour;
-            this.minute = minute;
-            this.seconds = seconds;
-        }
-
-        public TimeDuration() {
-
-        }
-
-        public int getHour() {
-            return hour;
-        }
-
-        public void setHour(int hour) {
-            this.hour = hour;
-        }
-
-        public int getMinute() {
-            return minute;
-        }
-
-        public void setMinute(int minute) {
-            this.minute = minute;
-        }
-
-        public int getSeconds() {
-            return seconds;
-        }
-
-        public void setSeconds(int seconds) {
-            this.seconds = seconds;
-        }
-
-        public long convertToMills() {
-            return (((hour * 3600) + (minute * 60)) * 1000);
-        }
-
-        private String hourToString() {
-            if (hour <= 0) {
-                return "";
-            }
-            return hour + "hours";
-        }
-
-        private String minuteToString() {
-            if (minute <= 0) {
-                return "";
-            }
-
-            return minute + "minutes";
-        }
-
-        public String timeToString() {
-            if (hour <= 0 && minute <= 0) {
-                return "No time set";
-            }
-            return hourToString() + ":" + minuteToString();
-        }
-
+    public void retreiveSettings() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String val = pref.getString(DEFAULT_VALUE, "0:5");
+        Log.d("SHARED PREF", val);
     }
+
 
 }
 
