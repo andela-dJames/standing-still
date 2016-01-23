61a5e5e52fcc71f04c59af06294444a2e0c4e2a2
andela-djames <daniel.james@andela.com>
2016-01-23 10:49:24 +0100

    Add new methods to clas

--- StandingStill/app/src/main/java/com/andela/standingstill/model/Place.java
+++ StandingStill/app/src/main/java/com/andela/standingstill/model/Place.java
@@ -3,7 +3,9 @@ package com.andela.standingstill.model;
 import org.joda.time.DateTime;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class Place {
@@ -84,5 +86,59 @@ public class Place {
 
     public void setDateVisited(DateTime dateVisited) {
         this.dateVisited = dateVisited;
+    }
+
+    public CharSequence getTimeSpentToString() {
+        long sec = timeSpent / 1000;
+        long min = sec / 60;
+        long hr = min / 60;
+        if (hr < 1){
+            return (min <= 1) ? min + " min" : min+ " mins";
+        }
+
+        return ((hr <= 1) ? hr + " hr" : hr + " hrs") + ((min <= 1) ? min + " min" : min+ " mins");
+
+    }
+
+    public static Map<String, List<Movement>> groupByDate(List<Movement> list){
+        Map<String, List<Movement>> group = new HashMap<>();
+        for (Movement movement: list){
+            String key = movement.getAddress();
+            if (!group.containsKey(key)){
+                List<Movement> places = new ArrayList<>();
+                places.add(movement);
+                group.put(key, places);
+            }
+            else {
+                group.get(key).add(movement);
+            }
+        }
+
+
+        return group;
+    }
+
+    public List<Place> getPlaces(List<Movement> movements) {
+        List<Place>places = new ArrayList<>();
+        Place p;
+        Map<String, List<Movement>> map;
+        map = groupByDate(movements);
+        for (String key: map.keySet()){
+            p = new Place();
+            p.setAddress(key);
+            p.setTimeSpent(totalTimeSpent(map.get(key)));
+            p.setMovementList(map.get(key));
+            places.add(p);
+        }
+
+        return places;
+    }
+
+    public long totalTimeSpent(List<Movement> movements){
+        long val = 0;
+        for (Movement movement : movements){
+            val += movement.getTimeSpent();
+        }
+        return val;
     }
 }
