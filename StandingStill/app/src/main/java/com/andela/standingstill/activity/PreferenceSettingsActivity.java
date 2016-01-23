31bdf4325455ceecf85006b6765e4d4929637400
andela-djames <daniel.james@andela.com>
2016-01-23 10:41:33 +0100

    Remove unused imports

--- StandingStill/app/src/main/java/com/andela/standingstill/activity/PreferenceSettingsActivity.java
+++ StandingStill/app/src/main/java/com/andela/standingstill/activity/PreferenceSettingsActivity.java
@@ -4,7 +4,6 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatDelegate;
