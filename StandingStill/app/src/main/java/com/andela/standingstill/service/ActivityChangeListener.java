9f8647c578a60a9e2292cc0cbc88f18e0f3501e0
andela-djames <daniel.james@andela.com>
2016-01-23 10:51:08 +0100

    Remove unused code

--- StandingStill/app/src/main/java/com/andela/standingstill/service/ActivityChangeListener.java
+++ StandingStill/app/src/main/java/com/andela/standingstill/service/ActivityChangeListener.java
@@ -1,9 +1,7 @@
 package com.andela.standingstill.service;
 
-/**
- * Created by andeladev on 13/01/2016.
- */
+
 public interface ActivityChangeListener {
 
-    public void onActivityChange(String name);
+     void onActivityChange(String name);
 }
